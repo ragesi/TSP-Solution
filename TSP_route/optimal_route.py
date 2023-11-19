7c31847a6d42fec97f3652a0d4c3b9815bd3439c
ragesi <1727570963@qq.com>
2023-11-19 23:33:42 +0800

    TSP for larger scales

--- TSP_route/optimal_route.py
+++ TSP_route/optimal_route.py
@@ -1,15 +1,16 @@
 # -*- coding: UTF-8 -*-
 
 from qiskit import QuantumRegister, ClassicalRegister, QuantumCircuit
+import qiskit.circuit.library as lib
 
 import numpy as np
 import math as m
 
-from utils import display_result, NOT_gate, unitary_function, util
+from utils import display_result, NOT_gate, unitary_function as uf, util
 
 
 class OptimalRoute:
-    def __init__(self, node_num, node_list, dis_adj):
+    def __init__(self, node_num, node_list, dis_adj, total_qubit_num):
         """
         :param node_num: the number of cities in the route
         :param node_list: the list of cities' id,
@@ -23,30 +24,34 @@ class OptimalRoute:
         # self.dis_adj's size is [node_num - 1, 4], filling 0 to make-up, which excludes the last step's distance
         self.dis_adj = np.zeros((self.node_num - 1, 4))
         self.end_dis = np.zeros(4)
+        self.total_qubit_num = total_qubit_num
+
         # the number of choices for every step, excluding the start and the end
         self.choice_num = self.node_num - 2
         # the number of steps that have choices
         self.step_num = self.node_num - 2
         # the number of choice encoding bits in binary
-        self.choice_code_num = m.ceil(1.0 * m.sqrt(self.choice_num))
+        self.choice_bit_num = m.ceil(1.0 * m.log2(self.choice_num))
+        self.qram_num = self.step_num * self.choice_bit_num
         # the precision of route distance
         self.precision = 6
+        self.anc_num = max(self.precision, self.choice_bit_num) - 1
         self.thresholds = []
-        self.illegal_choice_list = []
-        self.illegal_route_list = []
+        # self.illegal_choice_list = []
+        # self.illegal_route_list = []
 
-        self.q_choice = None
-        self.q_detail = None
-        self.q_ancilla = None
-        self.q_result = None
-        self.c = None
+        self.qram = None
+        self.tmp_info = None
+        self.anc = None
+        self.res = None
+        self.cl = None
         self.qc = None
 
         self.init_dis_adj(dis_adj)
-        self.init_Circuit()
-        # get the parameter of algorithm, which is used in each iteration
-        self.get_illegal_choice_param()
-        self.get_illegal_route_param()
+        self.init_circuit()
+        # # get the parameter of algorithm, which is used in each iteration
+        # self.get_illegal_choice_param()
+        # self.get_illegal_route_param()
 
     def init_dis_adj(self, dis_adj):
         """
@@ -58,11 +63,11 @@ class OptimalRoute:
         for i in np.arange(len(dis_adj)):
             for j in np.arange(i, len(dis_adj[i])):
                 total_distance += dis_adj[i][j]
-        # normalize the adjacency matrix to 2.0 * m.pi / 64
+        # normalize the adjacency matrix to 2.0 * m.pi / (2 ** precision)
         for i in np.arange(len(dis_adj)):
             for j in np.arange(len(dis_adj[i])):
-                dis_adj[i][j] = round(1.0 * dis_adj[i][j] / total_distance * 60)
-                dis_adj[i][j] = 2.0 * m.pi * dis_adj[i][j] / 64
+                dis_adj[i][j] = round(1.0 * dis_adj[i][j] / total_distance * (2 ** self.precision))
+                dis_adj[i][j] = 2.0 * m.pi * dis_adj[i][j] / (2 ** self.precision)
 
         # calculating the threshold
         threshold = 1.0 / (2 ** self.precision)
@@ -80,7 +85,7 @@ class OptimalRoute:
         # make up the rest of adjacency matrix
         self.dis_adj[:, :self.node_num - 2] = dis_adj
 
-    def init_Circuit(self):
+    def init_circuit(self):
         """
         init the quantum circuit for detecting the legal route, which has 4 parts
         1. choice register: store the choice of every step
@@ -88,90 +93,131 @@ class OptimalRoute:
         3. ancilla register: to assist the process of algorithm
         4. result register: to record the result of every stage of algorithm
         """
-        total_num = 27
+        if self.qram_num + self.step_num + self.anc_num + 4 > self.total_qubit_num:
+            raise ValueError("The number of nodes is too much!")
         # for the end node, it doesn't have choice; for the node_list[-2], it only has one choice for the end
-        choice_num = self.step_num * self.choice_code_num
-        detail_num = 6
-        result_num = 4
-        ancilla_num = total_num - choice_num - detail_num - result_num
-        print("choice_num: ", choice_num)
-        print("detail_num: ", detail_num)
-        print("ancilla_num: ", ancilla_num)
-        print("result_num: ", result_num)
+        self.anc_num = max(self.anc_num, self.total_qubit_num - self.qram_num - self.step_num - 4)
+        print("qram_num: ", self.qram_num)
+        print("tmp_info_num: ", self.step_num)
+        print("anc_num: ", self.anc_num)
+        print("res_num: ", 4)
 
-        self.q_choice = QuantumRegister(choice_num, name='choice')
-        self.q_detail = QuantumRegister(detail_num, name='detail')
-        self.q_ancilla = QuantumRegister(ancilla_num, name='ancilla')
-        self.q_result = QuantumRegister(result_num, name='result')
-        self.c = ClassicalRegister(choice_num + 1, name='c')
-        self.qc = QuantumCircuit(self.q_choice, self.q_detail, self.q_ancilla, self.q_result, self.c)
+        self.qram = QuantumRegister(self.qram_num, name='qram')
+        self.tmp_info = QuantumRegister(self.step_num, name='tmp_info')
+        self.anc = QuantumRegister(self.anc_num, name='anc')
+        self.res = QuantumRegister(4, name='res')
+        self.cl = ClassicalRegister(self.qram_num + 1, name='c')
+        self.qc = QuantumCircuit(self.qram, self.tmp_info, self.anc, self.res, self.cl)
 
-        for i in np.arange(choice_num):
-            self.qc.h(self.q_choice[i])
-        self.qc.x(self.q_result[3])
-        self.qc.h(self.q_result[3])
+        for i in np.arange(self.qram_num):
+            self.qc.h(self.qram[i])
+        self.qc.x(self.res[3])
+        self.qc.h(self.res[3])
 
-    def get_illegal_choice_param(self):
-        """
-        make up the list of illegal choice parameter, which is used in the process of is_legal_choice
-        """
-        size = 0
-        for i in np.arange(2 ** self.choice_code_num - 1, self.choice_num - 1, -1):
-            for j in np.arange(self.step_num):
-                self.illegal_choice_list.append(
-                    [self.q_choice[j * self.choice_code_num: (j + 1) * self.choice_code_num], i, self.q_detail[size]])
-                size += 1
+    # def get_illegal_choice_param(self):
+    #     """
+    #     make up the list of illegal choice parameter, which is used in the process of is_legal_choice
+    #     """
+    #     size = 0
+    #     for i in np.arange(2 ** self.choice_bit_num - 1, self.choice_num - 1, -1):
+    #         for j in np.arange(self.step_num):
+    #             self.illegal_choice_list.append(
+    #                 [self.q_choice[j * self.choice_bit_num: (j + 1) * self.choice_bit_num], i, self.q_detail[size]])
+    #             size += 1
+    #
+    # def get_illegal_route_param(self):
+    #     """
+    #     make up the list of illegal route parameter, which is used in the process of is_legal_route
+    #     """
+    #     size = 0
+    #     for i in np.arange(self.step_num):
+    #         for j in np.arange(i + 1, self.step_num):
+    #             self.illegal_route_list.append([self.q_choice[i * self.choice_bit_num: (i + 1) * self.choice_bit_num],
+    #                                             self.q_choice[j * self.choice_bit_num: (j + 1) * self.choice_bit_num],
+    #                                             self.q_detail[size]])
+    #             size += 1
 
-    def get_illegal_route_param(self):
+    def check_choice_validity(self):
         """
-        make up the list of illegal route parameter, which is used in the process of is_legal_route
+        determine whether the choice of every step is less than the number of nodes that can be chosen
+        :return: QuantumCircuit
         """
-        size = 0
+        qram = QuantumRegister(self.qram_num)
+        tmp_info = QuantumRegister(self.step_num)
+        anc = QuantumRegister(self.anc_num)
+        res = QuantumRegister(1)
+        qc = QuantumCircuit(qram, tmp_info, anc, res)
+
+        # integer comparator
         for i in np.arange(self.step_num):
-            for j in np.arange(i + 1, self.step_num):
-                self.illegal_route_list.append([self.q_choice[i * self.choice_code_num: (i + 1) * self.choice_code_num],
-                                                self.q_choice[j * self.choice_code_num: (j + 1) * self.choice_code_num],
-                                                self.q_detail[size]])
-                size += 1
+            qc.append(lib.IntegerComparator(self.choice_bit_num, self.choice_num, False),
+                      [*qram[i * self.choice_bit_num: (i + 1) * self.choice_bit_num], tmp_info[i],
+                       *anc[:(self.choice_bit_num - 1)]])
+        qc.append(NOT_gate.custom_mcx(self.step_num, self.anc_num), [*tmp_info, *anc, *res])
+        for i in np.arange(self.step_num - 1, -1, -1):
+            qc.append(lib.IntegerComparator(self.choice_bit_num, self.choice_num, False).inverse(),
+                      [*qram[i * self.choice_bit_num: (i + 1) * self.choice_bit_num], tmp_info[i],
+                       *anc[:(self.choice_bit_num - 1)]])
 
-    def is_legal_choice(self, dgr):
-        """
-        determine whether each step's choice exceeds the range of choice_list
-        :param dgr: determine whether it is the function's inverse operation or not
-        """
-        size = len(self.illegal_choice_list)
-        if dgr:
-            self.illegal_choice_list.reverse()
-        for i in np.arange(size):
-            NOT_gate.equal_to_int_NOT(self.qc, self.illegal_choice_list[i][0], self.illegal_choice_list[i][1],
-                                      self.illegal_choice_list[i][2], self.q_ancilla, self.choice_code_num)
+        return qc
 
-        NOT_gate.zero_NOT(self.qc, self.q_detail[0: size], self.q_result[0], self.q_ancilla, size)
+    # def is_legal_choice(self, dgr):
+    #     """
+    #     determine whether each step's choice exceeds the range of choice_list
+    #     :param dgr: determine whether it is the function's inverse operation or not
+    #     """
+    #     size = len(self.illegal_choice_list)
+    #     if dgr:
+    #         self.illegal_choice_list.reverse()
+    #     for i in np.arange(size):
+    #         NOT_gate.equal_to_int_NOT(self.qc, self.illegal_choice_list[i][0], self.illegal_choice_list[i][1],
+    #                                   self.illegal_choice_list[i][2], self.q_ancilla, self.choice_bit_num)
+    #
+    #     NOT_gate.zero_NOT(self.qc, self.q_detail[0: size], self.q_result[0], self.q_ancilla, size)
+    #
+    #     for i in np.arange(size - 1, -1, -1):
+    #         NOT_gate.equal_to_int_NOT(self.qc, self.illegal_choice_list[i][0], self.illegal_choice_list[i][1],
+    #                                   self.illegal_choice_list[i][2], self.q_ancilla, self.choice_bit_num)
+    #     if dgr:
+    #         self.illegal_choice_list.reverse()
 
-        for i in np.arange(size - 1, -1, -1):
-            NOT_gate.equal_to_int_NOT(self.qc, self.illegal_choice_list[i][0], self.illegal_choice_list[i][1],
-                                      self.illegal_choice_list[i][2], self.q_ancilla, self.choice_code_num)
-        if dgr:
-            self.illegal_choice_list.reverse()
+    def check_route_validity(self):
+        qram = QuantumRegister(self.qram_num)
+        tmp_info = QuantumRegister(self.step_num)
+        anc = QuantumRegister(self.anc_num)
+        res = QuantumRegister(1)
+        qc = QuantumCircuit(qram, tmp_info, anc, res)
 
-    def is_legal_route(self, dgr):
-        """
-        determine if the choices in the steps are different
-        """
-        size = len(self.illegal_route_list)
-        if dgr:
-            self.illegal_route_list.reverse()
-        for i in np.arange(size):
-            NOT_gate.equal_NOT(self.qc, self.illegal_route_list[i][0], self.illegal_route_list[i][1],
-                               self.illegal_route_list[i][2], self.q_ancilla, self.choice_code_num)
+        for i in np.arange(self.step_num):
+            for j in np.arange(self.choice_num):
+                qc.append(NOT_gate.equal_to_int_NOT(j, self.choice_bit_num, self.anc_num),
+                          [*qram[i * self.choice_bit_num: (i + 1) * self.choice_bit_num], *anc, tmp_info[j]])
+        qc.append(NOT_gate.custom_mcx(self.step_num, self.anc_num), [*tmp_info, *anc, *res])
+        for i in np.arange(self.step_num - 1):
+            for j in np.arange(self.choice_num - 1, -1, -1):
+                qc.append(NOT_gate.equal_to_int_NOT(j, self.choice_bit_num, self.anc_num).inverse(),
+                          [*qram[i * self.choice_bit_num: (i + 1) * self.choice_bit_num], *anc, tmp_info[j]])
 
-        NOT_gate.zero_NOT(self.qc, self.q_detail[0: size], self.q_result[1], self.q_ancilla, size)
+        return qc
 
-        for i in np.arange(size - 1, -1, -1):
-            NOT_gate.equal_NOT(self.qc, self.illegal_route_list[i][0], self.illegal_route_list[i][1],
-                               self.illegal_route_list[i][2], self.q_ancilla, self.choice_code_num)
-        if dgr:
-            self.illegal_route_list.reverse()
+    # def is_legal_route(self, dgr):
+    #     """
+    #     determine if the choices in the steps are different
+    #     """
+    #     size = len(self.illegal_route_list)
+    #     if dgr:
+    #         self.illegal_route_list.reverse()
+    #     for i in np.arange(size):
+    #         NOT_gate.equal_NOT(self.qc, self.illegal_route_list[i][0], self.illegal_route_list[i][1],
+    #                            self.illegal_route_list[i][2], self.q_ancilla, self.choice_bit_num)
+    #
+    #     NOT_gate.zero_NOT(self.qc, self.q_detail[0: size], self.q_result[1], self.q_ancilla, size)
+    #
+    #     for i in np.arange(size - 1, -1, -1):
+    #         NOT_gate.equal_NOT(self.qc, self.illegal_route_list[i][0], self.illegal_route_list[i][1],
+    #                            self.illegal_route_list[i][2], self.q_ancilla, self.choice_bit_num)
+    #     if dgr:
+    #         self.illegal_route_list.reverse()
 
     def cal_distance(self):
         """
